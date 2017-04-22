9a9dc4869b43da8da74e50cef417591fddeca9ea
Alex Khomchenko <akhomchenko@gmail.com>
2017-04-22 16:34:40 +0200

    #14 fix issue with banner option inheritance

--- src/index.js
+++ src/index.js
@@ -6,18 +6,24 @@ import { getCommentContent } from '@comandeer/babel-plugin-banner/utils';
 import { transform } from 'babel-core';
 
 function babili( options = {} ) {
+	let _banner;
+
 	return {
 		name: 'babili',
 
-		transformBundle( bundle, rollupOptions ) {
+		options( { banner } ) {
+			_banner = banner;
+		},
+
+		transformBundle( bundle ) {
 			const babelConf = {
 				presets: [ [ babiliPreset, options ] ],
 				sourceMaps: typeof options.sourceMap !== 'undefined' ? Boolean( options.sourceMap ) : true,
 				comments: typeof options.comments !== 'undefined' ? Boolean( options.comments ) : true
 			};
 
-			if ( typeof options.banner === 'string' || typeof rollupOptions.banner === 'string' ) {
-				const banner = options.banner || rollupOptions.banner;
+			if ( typeof options.banner === 'string' || typeof _banner === 'string' ) {
+				const banner = options.banner || _banner;
 				const bannerContent = getCommentContent( banner );
 				let isAlreadyInserted = false;
 
